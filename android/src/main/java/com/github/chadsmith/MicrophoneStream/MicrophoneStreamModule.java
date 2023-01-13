24e1905eb4352c0b0002b5dcffa4c6b27d62d9d0
Rahul Singh <raulsi@raulsi.in>
2023-01-13 13:55:20 +0900

    update android

--- android/src/main/java/com/github/chadsmith/MicrophoneStream/MicrophoneStreamModule.java
+++ android/src/main/java/com/github/chadsmith/MicrophoneStream/MicrophoneStreamModule.java
@@ -122,14 +122,10 @@ class MicrophoneStreamModule extends ReactContextBaseJavaModule {
 
     private void recording() {
         short buffer[] = new short[bufferSize];
-        byte encoded[] = new byte[bufferSize];
-        G711UCodec codec = new G711UCodec();
-
         while (running && !reactContext.getCatalystInstance().isDestroyed()) {
             WritableArray data = Arguments.createArray();
             audioRecord.read(buffer, 0, bufferSize);
-            codec.encode(buffer, bufferSize, encoded, 0);
-            for (byte value : encoded) {
+            for (float value : buffer) {
                 data.pushInt((int) value);
             }
             eventEmitter.emit("audioData", data);
