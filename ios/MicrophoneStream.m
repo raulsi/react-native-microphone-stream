ae9eeabe78d068acf7ad0c37f8c1201ef690ebf5
Rahul Singh <raulsi07@cloubot.com>
2023-03-22 13:32:57 +0900

    Update MicrophoneStream.m

--- ios/MicrophoneStream.m
+++ ios/MicrophoneStream.m
@@ -1,10 +1,13 @@
 #import "MicrophoneStream.h"
 
+#define BIAS (0x84)
+#define CLIP 8159
+
 @implementation MicrophoneStream {
     AudioQueueRef _queue;
     AudioQueueBufferRef _buffer;
-    NSNumber *_audioData[65536];
-    UInt32 _bufferSize;
+    AVAudioSessionCategory _category;
+    AVAudioSessionMode _mode;
 }
 
 void inputCallback(
@@ -17,11 +20,51 @@ void inputCallback(
     [(__bridge MicrophoneStream *) inUserData processInputBuffer:inBuffer queue:inAQ];
 }
 
+int seg_uend[8] = { 0x3F, 0x7F, 0xFF, 0x1FF, 0x3FF, 0x7FF, 0xFFF, 0x1FFF };
+
+static int search(int val, int *table, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (val <= *table++)
+            return (i);
+    }
+    return (size);
+}
+
+int linear2ulaw(int pcm_val) {
+    int mask;
+    int seg;
+    int uval;
+
+    pcm_val = pcm_val >> 2;
+    if (pcm_val < 0) {
+        pcm_val = -pcm_val;
+        mask = 0x7F;
+    } else {
+        mask = 0xFF;
+    }
+    if (pcm_val > CLIP)
+        pcm_val = CLIP;
+    pcm_val += (BIAS >> 2);
+
+    seg = search(pcm_val, seg_uend, 8);
+
+    if (seg >= 8)
+        return (0x7F ^ mask);
+    else {
+        uval = (seg << 4) | ((pcm_val >> (seg + 1)) & 0xF);
+        return (uval ^ mask);
+    }
+}
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    _category = [session category];
+    _mode = [session mode];
+
     UInt32 bufferSize = options[@"bufferSize"] == nil ? 8192 : [options[@"bufferSize"] unsignedIntegerValue];
-    _bufferSize = bufferSize;
 
     AudioStreamBasicDescription description;
     description.mReserved = 0;
@@ -40,25 +83,43 @@ RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
 }
 
 RCT_EXPORT_METHOD(start) {
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord
+                   error:nil];
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker
+                               error:nil];
     AudioQueueStart(_queue, NULL);
 }
 
 RCT_EXPORT_METHOD(pause) {
     AudioQueuePause(_queue);
     AudioQueueFlush(_queue);
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:_category
+                   error:nil];
+    [session setMode:_mode
+               error:nil];
 }
 
 RCT_EXPORT_METHOD(stop) {
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:_category
+                   error:nil];
+    [session setMode:_mode
+               error:nil];
     AudioQueueStop(_queue, YES);
 }
 
 - (void)processInputBuffer:(AudioQueueBufferRef)inBuffer queue:(AudioQueueRef)queue {
     SInt16 *audioData = inBuffer->mAudioData;
     UInt32 count = inBuffer->mAudioDataByteSize / sizeof(SInt16);
-    for (int i = 0; i < _bufferSize; i++) {
-        _audioData[i] = @(audioData[i]);
-    }
-    [self sendEventWithName:@"audioData" body:[NSArray arrayWithObjects:_audioData count:count]];
+
+    NSMutableArray *array  = [NSMutableArray arrayWithCapacity:count];
+
+    for (int i = 0; i < count; ++i)
+        array[i] = @(audioData[i]);
+
+    [self sendEventWithName:@"audioData" body:array];
     AudioQueueEnqueueBuffer(queue, inBuffer, 0, NULL);
 }
 
